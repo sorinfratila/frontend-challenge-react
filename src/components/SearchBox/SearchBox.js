b8190a447a9479cc613b13a27449236d07699e24
sorinfratila <sorin.fratila@protonmail.ch>
2020-05-28 14:34:46 +0200

    added debounce lodash function for submitting search result

--- src/components/SearchBox/SearchBox.js
+++ src/components/SearchBox/SearchBox.js
@@ -1,15 +1,28 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { debounce } from 'lodash';
 import classes from './SearchBox.module.scss';
 
 function SearchBox() {
   const [value, setValue] = useState('');
+
+  const callAPI = () => {
+    console.log('asdfasdf');
+  };
+
+  const [debouncedCallAPI] = useState(() => debounce(callAPI, 250));
+
+  function handleChange(value) {
+    setValue(value);
+    debouncedCallAPI();
+  }
+
   return (
     <input
       className={classes.search}
       type="search"
       placeholder="Search"
-      onChange={e => setValue(e.target.value)}
+      onChange={e => handleChange(e.target.value)}
       value={value}
     />
   );
